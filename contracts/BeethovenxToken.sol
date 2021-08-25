389d58ffde7207cb54ddd42046f119e491adb681
beethovenxfi <beethovenxfi@gmail.com>
2021-08-25 18:54:44 +0200

    checkpoint make it compile

--- contracts/BeethovenxToken.sol
+++ contracts/BeethovenxToken.sol
@@ -2,8 +2,12 @@
 
 pragma solidity 0.6.12;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BeethovenxToken is ERC20("BeethovenxToken", "BEETX"), Ownable {
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
 }
--- contracts/interfaces/IRewarder.sol
+++ contracts/interfaces/IRewarder.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.12;
-import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
 interface IRewarder {
-    using BoringERC20 for IERC20;
     function onBeetxReward(uint256 pid, address user, address recipient, uint256 sushiAmount, uint256 newLpAmount) external;
-    function pendingTokens(uint256 pid, address user, uint256 beetxAmount) external view returns (IERC20[] memory, uint256[] memory);
+//    function pendingTokens(uint256 pid, address user, uint256 beetxAmount) external view returns (IERC20[] memory, uint256[] memory);
 }
