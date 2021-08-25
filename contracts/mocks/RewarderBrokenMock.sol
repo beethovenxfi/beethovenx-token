face2ae0ba9d32a9e28f32cd7a04a57dcefc1a44
0xSkly <skly.beets@protonmail.com>
2021-08-25 21:51:48 +0200

    make first test pass

--- contracts/mocks/RewarderBrokenMock.sol
+++ contracts/mocks/RewarderBrokenMock.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
 
-//// SPDX-License-Identifier: MIT
-//
-//pragma solidity 0.6.12;
-//import "../interfaces/IRewarder.sol";
-//
-//
-//contract RewarderBrokenMock is IRewarder {
-//
-//    function onBeetxReward (uint256, address, address, uint256, uint256) override external {
-//        revert();
-//    }
-//
-////    function pendingTokens(uint256 pid, address user, uint256 sushiAmount) override external view returns (IERC20[] memory rewardTokens, uint256[] memory rewardAmounts){
-////        revert();
-////    }
-//
-//}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+import "../interfaces/IRewarder.sol";
+
+
+contract RewarderBrokenMock is IRewarder {
+
+    function onBeetxReward (uint256, address, address, uint256, uint256) override external {
+        revert();
+    }
+
+    function pendingTokens(uint256 pid, address user, uint256 beetxAmount) override external view returns (IERC20[] memory rewardTokens, uint256[] memory rewardAmounts){
+        revert();
+    }
+
+}
--- contracts/mocks/RewarderMock.sol
+++ contracts/mocks/RewarderMock.sol
@@ -1,53 +1,49 @@
 // SPDX-License-Identifier: MIT
 
-//// SPDX-License-Identifier: MIT
-//
-//pragma solidity 0.6.12;
-//import "../interfaces/IRewarder.sol";
-//import {BoringERC20} from "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
-//import "@boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol";
-////import "../interfaces/IERC20.sol";
-////import {SafeERC20} from "../libraries/SafeERC20.sol";
-//
-//
-//contract RewarderMock is IRewarder {
-//    using BoringMath for uint256;
-//    using BoringERC20 for IERC20;
-//    uint256 private immutable rewardMultiplier;
-//    IERC20 private immutable rewardToken;
-//    uint256 private constant REWARD_TOKEN_DIVISOR = 1e18;
-//    address private immutable MASTERCHEF_V2;
-//
-//    constructor (uint256 _rewardMultiplier, IERC20 _rewardToken, address _MASTERCHEF_V2) public {
-//        rewardMultiplier = _rewardMultiplier;
-//        rewardToken = _rewardToken;
-//        MASTERCHEF_V2 = _MASTERCHEF_V2;
-//    }
-//
-//    function onBeetxReward (uint256, address user, address to, uint256 sushiAmount, uint256) onlyMCV2 override external {
-//        uint256 pendingReward = sushiAmount.mul(rewardMultiplier) / REWARD_TOKEN_DIVISOR;
-//        uint256 rewardBal = rewardToken.balanceOf(address(this));
-//        if (pendingReward > rewardBal) {
-//            rewardToken.safeTransfer(to, rewardBal);
-//        } else {
-//            rewardToken.safeTransfer(to, pendingReward);
-//        }
-//    }
-//
-//    function pendingTokens(uint256 pid, address user, uint256 sushiAmount) override external view returns (IERC20[] memory rewardTokens, uint256[] memory rewardAmounts) {
-//        IERC20[] memory _rewardTokens = new IERC20[](1);
-//        _rewardTokens[0] = (rewardToken);
-//        uint256[] memory _rewardAmounts = new uint256[](1);
-//        _rewardAmounts[0] = sushiAmount.mul(rewardMultiplier) / REWARD_TOKEN_DIVISOR;
-//        return (_rewardTokens, _rewardAmounts);
-//    }
-//
-//    modifier onlyMCV2 {
-//        require(
-//            msg.sender == MASTERCHEF_V2,
-//            "Only MCV2 can call this function."
-//        );
-//        _;
-//    }
-//
-//}
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+import "../interfaces/IRewarder.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
+contract RewarderMock is IRewarder {
+    using SafeERC20 for IERC20;
+    uint256 private immutable rewardMultiplier;
+    IERC20 private immutable rewardToken;
+    uint256 private constant REWARD_TOKEN_DIVISOR = 1e18;
+    address private immutable BEETHOVEN_MASTERCHEF;
+
+    constructor (uint256 _rewardMultiplier, IERC20 _rewardToken, address _BEETHOVEN_MASTERCHEF) public {
+        rewardMultiplier = _rewardMultiplier;
+        rewardToken = _rewardToken;
+        BEETHOVEN_MASTERCHEF =  _BEETHOVEN_MASTERCHEF;
+    }
+
+    function onBeetxReward (uint256, address user, address to, uint256 beetxAmount, uint256) onlyMCV2 override external {
+        uint256 pendingReward = beetxAmount * rewardMultiplier / REWARD_TOKEN_DIVISOR;
+        uint256 rewardBal = rewardToken.balanceOf(address(this));
+        if (pendingReward > rewardBal) {
+            rewardToken.safeTransfer(to, rewardBal);
+        } else {
+            rewardToken.safeTransfer(to, pendingReward);
+        }
+    }
+
+    function pendingTokens(uint256 pid, address user, uint256 beetxAmount) override external view returns (IERC20[] memory rewardTokens, uint256[] memory rewardAmounts) {
+        IERC20[] memory _rewardTokens = new IERC20[](1);
+        _rewardTokens[0] = (rewardToken);
+        uint256[] memory _rewardAmounts = new uint256[](1);
+        _rewardAmounts[0] = beetxAmount * rewardMultiplier / REWARD_TOKEN_DIVISOR;
+        return (_rewardTokens, _rewardAmounts);
+    }
+
+    modifier onlyMCV2 {
+        require(
+            msg.sender == BEETHOVEN_MASTERCHEF,
+            "Only MCV2 can call this function."
+        );
+        _;
+    }
+
+}
