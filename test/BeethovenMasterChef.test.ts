e3d879e1102cf5d8a903668bd00d8fb20f70d6b3
curdin0xBeets <curdin.beets@protonmail.com>
2021-09-29 07:40:56 +0200

    wip

--- test/BeethovenMasterChef.test.ts
+++ test/BeethovenMasterChef.test.ts
@@ -1,9 +1,9 @@
 import { expect } from "chai"
-import { advanceBlock, advanceBlockTo, bn, deployChef, deployContract, deployERC20Mock } from "./utilities"
+import { advanceBlock, advanceBlockTo, bn, deployChef, deployContract, deployERC20Mock, setAutomineBlocks } from "./utilities"
 import { ethers } from "hardhat"
 import { BeethovenxMasterChef, BeethovenxToken, RewarderMock } from "../types"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
-import { BigNumber, BigNumberish } from "ethers"
+import { BigNumber } from "ethers"
 
 describe("BeethovenxMasterChef", function () {
   let beets: BeethovenxToken
@@ -280,172 +280,205 @@ describe("BeethovenxMasterChef", function () {
     await advanceBlockTo("209")
     expect(await beets.totalSupply()).to.equal(0)
   })
-  //
-  // it("distributes BEETS properly for each staker", async function () {
-  //   // 100 per block farming rate starting at block 300
-  //   // we give 20% to devs & 20% to treasury, so 60% are distributed to lp holders
-  //
-  //   /*
-  //       formula for rewards: FractionOfTotalLps * NumberOfBlocks * RewardsPerBlock * PercentageOfRewardsForPool
-  //       where RewardsPerBlock = 1000 & FractionOfRewardsForPool = 60%
-  //
-  //       we play the following scenario:
-  //       block 310 - alice deposit 10 LP
-  //       block 314 - bob deposits 20 LP
-  //        => alice rewards : 1 * 4 * 1000 * 0.6
-  //       block 318 - carol deposits 20 LPs
-  //        => alice rewards = prevRewards + 1/3 * 4 * 1000 * 0.6
-  //           bob rewards = 2/3 * 4 * 1000 * 0.6
-  //        ....
-  //    */
-  //   const chef = await deployChef(beets.address, dev.address, treasury.address, marketing.address, 1000, 300)
-  //   await beets.transferOwnership(chef.address)
-  //
-  //   const lp = await deployERC20Mock("Lp 1", "lp1", 10_000)
-  //   await lp.transfer(alice.address, "1000")
-  //   await lp.transfer(bob.address, "1000")
-  //   await lp.transfer(carol.address, "1000")
-  //
-  //   await chef.add("100", lp.address, ethers.constants.AddressZero)
-  //
-  //   await lp.connect(alice).approve(chef.address, "1000")
-  //   await lp.connect(bob).approve(chef.address, "1000")
-  //   await lp.connect(carol).approve(chef.address, "1000")
-  //   await advanceBlockTo("309")
-  //   // Alice deposits 10 LPs at block 310
-  //   await chef.connect(alice).deposit(0, "10", alice.address)
-  //   await advanceBlockTo("312")
-  //   await chef.connect(alice).harvest(0, alice.address) // block 313
-  //   expect(await beets.balanceOf(alice.address)).to.equal(3 * 1000 * 0.6)
-  //   // Bob deposits 20 LPs at block 314
-  //
-  //   await chef.connect(bob).deposit(0, "20", bob.address) //314
-  //   await advanceBlockTo("315")
-  //
-  //   // we disable automine so we can do both harvest calls in 1 block
-  //   await setAutomineBlocks(false)
-  //
-  //   await chef.connect(alice).harvest(0, alice.address) // block 316
-  //   await chef.connect(bob).harvest(0, bob.address) // block 316
-  //
-  //   await advanceBlockTo("316")
-  //   await setAutomineBlocks(true)
-  //   // expect(await beets.balanceOf(alice.address)).to.equal(4 * 1000 * 0.6 + (1000 / 3) * 0.6))
-  //   // alice should have 4 * 1000 * 0.6 + 2* 1000 / 3 * 0.6 = 2800
-  //   expect(await beets.balanceOf(alice.address)).to.equal(2800)
-  //   // bob should have  2 * 1000 * (2 / 3) * 0.6 = 800
-  //   expect(await beets.balanceOf(bob.address)).to.equal(800)
-  //
-  //   // Carol deposits 30 LPs at block 318
-  //   await chef.connect(carol).deposit(0, "30", carol.address) // block 317
-  //   await advanceBlockTo("319")
-  //
-  //   await chef.connect(alice).harvest(0, alice.address) // block 320
-  //   // await chef.connect(bob).harvest(0, alice.address) // block 320
-  //   // await chef.connect(carol).harvest(0, alice.address) // block 320
-  //
-  //   /*
-  //     alice (all harvested):
-  //       preVal + 1 block with 1/3 rewards + 3 blocks 1/6 of the rewards
-  //       2800 + (1 * 1000 * 1/3 * 0.6) + (3 * 1000 * 1/6 * 0.6) = 3300
-  //
-  //    bob (only preVal harvested, rest pending on master chef):
-  //     preVal + 1 block with 2/3 rewards + 3 blocks 2/6 rewards
-  //     prevVal: 800  |||  pending:  (1 * 1000 * 2/3 * 0.6) + ( 3 * 1000 * 2/6 * 0.6) = 1000 (total: 1800)
-  //
-  //    carol (everything pending on master chef):
-  //       3 blocks with 3/6 rewards
-  //       pending: 3 * 1000 * 3/6 * 0.6 = 900
-  //  */
-  //
-  //   expect(await beets.totalSupply()).to.equal(10_000)
-  //   expect(await beets.balanceOf(alice.address)).to.equal(3300)
-  //   // bob should still only have his 800 from the last harvest
-  //   expect(await beets.balanceOf(bob.address)).to.equal(800)
-  //   // carol has harvested nothing yet
-  //   expect(await beets.balanceOf(carol.address)).to.equal(0)
-  //   // all unharvested rewards are on the chef => total supply - alice balance - bob balance - dev balance - treasury balance
-  //   expect(await beets.balanceOf(chef.address)).to.equal(10_000 - 3300 - 800 - 2000 - 2000)
-  //
-  //   // 20% of all token rewards should have gone to dev
-  //   expect(await beets.balanceOf(dev.address)).to.equal(2000)
-  //
-  //   // 20% of all token rewards should have gone to treasury
-  //   expect(await beets.balanceOf(treasury.address)).to.equal(2000)
-  //
-  //   // alice deposits 10 more LP's
-  //   await chef.connect(alice).deposit(0, "10", alice.address) // block 321
-  //   await advanceBlockTo("329")
-  //
-  //   // Bob withdraws 5 LPs
-  //   await chef.connect(bob).withdrawAndHarvest(0, "5", bob.address) // block 330
-  //
-  //   /*
-  //     alice (parts harvested, parts pending):
-  //       preVal + 1 block 1/6 of the rewards + 9 blocks 2/7 of the rewards
-  //       harvested: 3300 ||  pending: 1 * 1000 * 1/6 * 0.6 + 9 * 1000 * 2/7 * 0.6 = 1642.857142857142857142 (total: 4942.857142857142857142)
-  //
-  //    bob (all harvested):
-  //     preVal + 1 block 2/6 rewards + 9 blocks 2/7 rewards
-  //     harvested: 1800 + 1 * 1000 * 2/6 * 0.6 + 9 * 1000 * 2/7 * 0.6 = 3542.857142857142857142
-  //
-  //    carol (everything pending on master chef):
-  //     preval + 10 blocks 3/6 rewards
-  //     pending: 900 + 1 * 1000 * 3/6 * 0.6 + 9 * 1000 * 3/7 * 0.6 = 3514.285714285714 => 3514
-  //  */
-  //
-  //   // take note that the decimals are just cut off (we are not adding the precision cause of javascript overflows of the number
-  //   expect(await beets.totalSupply()).to.equal(20_000)
-  //   expect(await beets.balanceOf(alice.address)).to.equal(3300)
-  //   expect(await beets.balanceOf(bob.address)).to.equal(3542)
-  //   expect(await beets.balanceOf(carol.address)).to.equal(0)
-  //   expect(await beets.balanceOf(chef.address)).to.equal(5158)
-  //   expect(await beets.balanceOf(dev.address)).to.equal(4000)
-  //   expect(await beets.balanceOf(treasury.address)).to.equal(4000)
-  //
-  //   await advanceBlockTo("339")
-  //   // we only withdraw but dont harvest
-  //   await chef.connect(alice).withdrawAndHarvest(0, 20, alice.address) // block 340
-  //   /*
-  //     alice (all harvested):
-  //       preVal + 10 blocks 4/13 of the rewards
-  //       4942.857142857142857142 +  10 * 1000 * 4/13 * 0.6 = 6789.010989010989010988153846 => 6789
-  //   */
-  //   expect(await beets.balanceOf(alice.address)).to.equal(6789)
-  //   // await chef.connect(alice).harvest(0, alice.address) // block 340
-  //
-  //   // expect(await beets.balanceOf(alice.address)).to.equal(6788)
-  //
-  //   await advanceBlockTo("349")
-  //
-  //   await chef.connect(bob).withdrawAndHarvest(0, 15, bob.address) // block 350
-  //   /*
-  //     bob (all harvested):
-  //       preVal + 10 blocks 3/13 of the rewards + 10 blocks 1/3 of rewards
-  //        3542 + 10 * 1000 * 3/13 * 0.6 + 10 * 1000 * 1/3 * 0.6 = 6926.47252747 => 6926
-  //   */
-  //
-  //   expect(await beets.balanceOf(bob.address)).to.equal(6926)
-  //
-  //   await advanceBlockTo("359")
-  //
-  //   await chef.connect(carol).withdrawAndHarvest(0, 30, carol.address) // block 360
-  //   /*
-  //     carol (all harvested):
-  //       preVal + 10 blocks 6/13 of the rewards + 10 blocks 7/10 of rewards + 10 blocks 10/10 of rewards
-  //       3514 + 10 * 1000 * 6/13 * 0.6 + 10 * 1000 * 2/3 * 0.6 + 10 * 1000 * 0.6 = 16283.23076923077 => 16283
-  //   */
-  //   expect(await beets.balanceOf(carol.address)).to.equal(16283)
-  //
-  //   expect(await beets.totalSupply()).to.equal(50_000)
-  //   expect(await beets.balanceOf(dev.address)).to.equal(10_000)
-  //   expect(await beets.balanceOf(treasury.address)).to.equal(10_000)
-  //   // All of them should have 1000 LPs back.
-  //   expect(await lp.balanceOf(alice.address)).to.equal(1000)
-  //   expect(await lp.balanceOf(bob.address)).to.equal(1000)
-  //   expect(await lp.balanceOf(carol.address)).to.equal(1000)
-  // })
-  //
+
+  it("distributes BEETS properly for each staker", async function () {
+    // 100 per block farming rate starting at block 300
+    // we give 20% to devs & 20% to treasury, so 60% are distributed to lp holders
+
+    /*
+        formula for rewards: FractionOfTotalLps * NumberOfBlocks * RewardsPerBlock * PercentageOfRewardsForPool
+        where RewardsPerBlock = 1000 & FractionOfRewardsForPool = 60%
+
+        we play the following scenario:
+        block 310 - alice deposit 10 LP
+        block 314 - bob deposits 20 LP
+         => alice rewards : 1 * 4 * 1000 * 0.6
+        block 318 - carol deposits 20 LPs
+         => alice rewards = prevRewards + 1/3 * 4 * 1000 * 0.6
+            bob rewards = 2/3 * 4 * 1000 * 0.6
+         ....
+     */
+    const beetsPerBlock = bn(1000)
+    const chef = await deployChef(beets.address, dev.address, treasury.address, marketing.address, beetsPerBlock, 300)
+    await beets.transferOwnership(chef.address)
+
+    const lp = await deployERC20Mock("Lp 1", "lp1", 10_000)
+    await lp.transfer(alice.address, "1000")
+    await lp.transfer(bob.address, "1000")
+    await lp.transfer(carol.address, "1000")
+
+    await chef.add("100", lp.address, ethers.constants.AddressZero)
+
+    await lp.connect(alice).approve(chef.address, bn(1000))
+    await lp.connect(bob).approve(chef.address, bn(1000))
+    await lp.connect(carol).approve(chef.address, bn(1000))
+    await advanceBlockTo("309")
+
+    // Alice deposits 10 LPs at block 310
+    await chef.connect(alice).deposit(0, "10", alice.address)
+
+    await advanceBlockTo("312")
+    await chef.connect(alice).harvest(0, alice.address) // block 313
+    expect(await beets.balanceOf(alice.address)).to.equal(percentageOf(beetsPerBlock.mul(3), lpPercentage))
+    // Bob deposits 20 LPs at block 314
+
+    await chef.connect(bob).deposit(0, "20", bob.address) //314
+    await advanceBlockTo("315")
+
+    // we disable automine so we can do both harvest calls in 1 block
+    await setAutomineBlocks(false)
+
+    await chef.connect(alice).harvest(0, alice.address) // block 316
+    await chef.connect(bob).harvest(0, bob.address) // block 316
+
+    await advanceBlockTo("316")
+    await setAutomineBlocks(true)
+    // expect(await beets.balanceOf(alice.address)).to.equal(4 * 1000 * 0.6 + (1000 / 3) * 0.6))
+    // alice should have 4 * bPB * lpPc + 2 * bPb / 3 * lpPc
+    const aliceBalance316 = percentageOf(beetsPerBlock.mul(4), lpPercentage).add(percentageOf(beetsPerBlock.mul(2).div(3), lpPercentage))
+    expect(await beets.balanceOf(alice.address)).to.equal(aliceBalance316)
+    // bob should have  2 * bPb * (2 / 3) * lpPerc
+    const bobBalance316 = percentageOf(beetsPerBlock.mul(2).mul(2).div(3), lpPercentage)
+    expect(await beets.balanceOf(bob.address)).to.equal(bobBalance316)
+
+    // Carol deposits 30 LPs at block 318
+    await chef.connect(carol).deposit(0, "30", carol.address) // block 317
+    await advanceBlockTo("319")
+
+    await chef.connect(alice).harvest(0, alice.address) // block 320
+
+    /*
+      alice (all harvested):
+        preVal + 1 block with 1/3 rewards + 3 blocks 1/6 of the rewards
+        2800 + (1 * 1000 * 1/3 * 0.6) + (3 * 1000 * 1/6 * 0.6) = 3300
+
+     bob (only preVal harvested, rest pending on master chef):
+      preVal + 1 block with 2/3 rewards + 3 blocks 2/6 rewards
+      prevVal: 800  |||  pending:  (1 * 1000 * 2/3 * 0.6) + ( 3 * 1000 * 2/6 * 0.6) = 1000 (total: 1800)
+
+     carol (everything pending on master chef):
+        3 blocks with 3/6 rewards
+        pending: 3 * 1000 * 3/6 * 0.6 = 900
+   */
+
+    expect(await beets.totalSupply()).to.equal(bn(10_000))
+
+    // console.log("current balance", aliceBalance.toString())
+    const aliceBalance320 = aliceBalance316
+      .add(percentageOf(beetsPerBlock, lpPercentage).div(3))
+      .add(percentageOf(beetsPerBlock, lpPercentage).mul(3).div(6))
+
+    expect(await beets.balanceOf(alice.address)).to.equal(aliceBalance320)
+
+    // bob should still only have his 800 from the last harvest
+    expect(await beets.balanceOf(bob.address)).to.equal(bobBalance316)
+    // carol has harvested nothing yet
+    expect(await beets.balanceOf(carol.address)).to.equal(0)
+    // all unharvested rewards are on the chef => total supply - alice balance - bob balance - dev balance - treasury balance
+    const devBalance = percentageOf(beetsPerBlock.mul(10), devPercentage)
+    const treasuryBalance = percentageOf(beetsPerBlock.mul(10), treasuryPercentage)
+    const marketingBalance = percentageOf(beetsPerBlock.mul(10), marketingPercentage)
+    expect(await beets.balanceOf(chef.address)).to.equal(
+      bn(10_000).sub(aliceBalance320).sub(bobBalance316).sub(devBalance).sub(treasuryBalance).sub(marketingBalance)
+    )
+
+    expect(await beets.balanceOf(dev.address)).to.equal(devBalance)
+    expect(await beets.balanceOf(treasury.address)).to.equal(treasuryBalance)
+    expect(await beets.balanceOf(marketing.address)).to.equal(marketingBalance)
+
+    // alice deposits 10 more LP's
+    // await chef.connect(alice).deposit(0, "10", alice.address) // block 321
+    await advanceBlockTo("329")
+
+    // Bob withdraws 5 LPs
+    await chef.connect(bob).withdrawAndHarvest(0, "5", bob.address) // block 330
+
+    /*
+      alice (parts harvested, parts pending):
+        preVal + 1 block 1/6 of the rewards + 9 blocks 2/7 of the rewards
+        harvested: 3300 ||  pending: 1 * 1000 * 1/6 * 0.6 + 9 * 1000 * 2/7 * 0.6 = 1642.857142857142857142 (total: 4942.857142857142857142)
+
+     bob (all harvested):
+      preVal + 1 block 2/6 rewards + 9 blocks 2/7 rewards
+      harvested: 1800 + 1 * 1000 * 2/6 * 0.6 + 9 * 1000 * 2/7 * 0.6 = 3542.857142857142857142
+
+     carol (everything pending on master chef):
+      preval + 10 blocks 3/6 rewards
+      pending: 900 + 1 * 1000 * 3/6 * 0.6 + 9 * 1000 * 3/7 * 0.6 = 3514.285714285714 => 3514
+   */
+
+    // take note that the decimals are just cut off (we are not adding the precision cause of javascript overflows of the number
+    const aliceBalance330 = aliceBalance320
+      .add(percentageOf(beetsPerBlock.div(6), lpPercentage))
+      .add(percentageOf(beetsPerBlock.mul(9).mul(2).div(7), lpPercentage))
+
+    expect(await beets.totalSupply()).to.equal(bn(20_000))
+    expect(await beets.balanceOf(alice.address)).to.equal(aliceBalance320)
+    const bobBalance330 = bobBalance316
+      .add(percentageOf(beetsPerBlock.mul(2).div(6), lpPercentage))
+      .add(percentageOf(beetsPerBlock.mul(9).mul(2).div(7), lpPercentage))
+    expect(await beets.balanceOf(bob.address)).to.equal(bobBalance330)
+    expect(await beets.balanceOf(carol.address)).to.equal(0)
+
+    const devBalance330 = percentageOf(beetsPerBlock.mul(20), devPercentage)
+    const treasuryBalance330 = percentageOf(beetsPerBlock.mul(20), treasuryPercentage)
+    const marketingBalance330 = percentageOf(beetsPerBlock.mul(20), marketingPercentage)
+    expect(await beets.balanceOf(chef.address)).to.equal(
+      bn(10_000).sub(aliceBalance330).sub(bobBalance330).sub(devBalance330).sub(treasuryBalance330).sub(marketingBalance330)
+    )
+
+    expect(await beets.balanceOf(dev.address)).to.equal(devBalance)
+    expect(await beets.balanceOf(treasury.address)).to.equal(treasuryBalance)
+    expect(await beets.balanceOf(marketing.address)).to.equal(marketingBalance)
+
+    expect(await beets.balanceOf(chef.address)).to.equal(bn(5158))
+    expect(await beets.balanceOf(dev.address)).to.equal(bn(4000))
+    expect(await beets.balanceOf(treasury.address)).to.equal(bn(4000))
+
+    await advanceBlockTo("339")
+    // we only withdraw but dont harvest
+    await chef.connect(alice).withdrawAndHarvest(0, 20, alice.address) // block 340
+    /*
+      alice (all harvested):
+        preVal + 10 blocks 4/13 of the rewards
+        4942.857142857142857142 +  10 * 1000 * 4/13 * 0.6 = 6789.010989010989010988153846 => 6789
+    */
+    expect(await beets.balanceOf(alice.address)).to.equal(bn(6789))
+    // await chef.connect(alice).harvest(0, alice.address) // block 340
+
+    // expect(await beets.balanceOf(alice.address)).to.equal(6788)
+
+    await advanceBlockTo("349")
+
+    await chef.connect(bob).withdrawAndHarvest(0, 15, bob.address) // block 350
+    /*
+      bob (all harvested):
+        preVal + 10 blocks 3/13 of the rewards + 10 blocks 1/3 of rewards
+         3542 + 10 * 1000 * 3/13 * 0.6 + 10 * 1000 * 1/3 * 0.6 = 6926.47252747 => 6926
+    */
+
+    expect(await beets.balanceOf(bob.address)).to.equal(bn(6926))
+
+    await advanceBlockTo("359")
+
+    await chef.connect(carol).withdrawAndHarvest(0, 30, carol.address) // block 360
+    /*
+      carol (all harvested):
+        preVal + 10 blocks 6/13 of the rewards + 10 blocks 7/10 of rewards + 10 blocks 10/10 of rewards
+        3514 + 10 * 1000 * 6/13 * 0.6 + 10 * 1000 * 2/3 * 0.6 + 10 * 1000 * 0.6 = 16283.23076923077 => 16283
+    */
+    expect(await beets.balanceOf(carol.address)).to.equal(bn(16283))
+
+    expect(await beets.totalSupply()).to.equal(bn(50_000))
+    expect(await beets.balanceOf(dev.address)).to.equal(percentageOf(bn(50_000), devPercentage))
+    expect(await beets.balanceOf(treasury.address)).to.equal(percentageOf(bn(10_000), treasuryPercentage))
+    expect(await beets.balanceOf(marketing.address)).to.equal(percentageOf(bn(10_000), marketingPercentage))
+    // All of them should have 1000 LPs back.
+    expect(await lp.balanceOf(alice.address)).to.equal(1000)
+    expect(await lp.balanceOf(bob.address)).to.equal(1000)
+    expect(await lp.balanceOf(carol.address)).to.equal(1000)
+  })
+
   // it("gives correct BEETS allocation to each pool", async function () {
   //   /*
   //     1000 beets per block, start block 100, 20% dev & treadury each
@@ -484,6 +517,12 @@ describe("BeethovenxMasterChef", function () {
   //   expect(await chef.pendingBeets(1, bob.address)).to.equal(2250)
   // })
 })
+
+function rewardsCalculator(beetsPerBlock: BigNumber, percentage: number) {
+  return (blocks: number) => {
+    return percentageOf(beetsPerBlock.mul(blocks),percentage)
+  }
+}
 
 function percentageOf(value: BigNumber, percentage: number) {
   return value.mul(percentage).div(1000)
