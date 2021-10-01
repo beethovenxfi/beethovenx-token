420510c8c89c17029571d2ff3f7c8091184ab09b
0xSkly <skly.beets@protonmail.com>
2021-10-01 10:48:34 +0200

    add introduction text

--- contracts/token/BeethovenxMasterChef.sol
+++ contracts/token/BeethovenxMasterChef.sol
@@ -7,6 +7,18 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./BeethovenxToken.sol";
 import "../interfaces/IRewarder.sol";
 
+/*
+    This master chef is based on SUSHI's version with some adjustments:
+     - Upgrade to pragma 0.8.7
+     - therefore remove usage of SafeMath (built in overflow check for solidity > 8)
+     - Merge sushi's master chef V1 & V2 (no usage of dummy pool)
+     - remove withdraw function (without harvest) => requires the rewardDebt to be an signed int instead of uint which requires a lot of casting and has no real usecase for us
+     - no dev emissions, but treasury emissions instead
+     - treasury percentage is subtracted from emissions instead of added on top
+     - update of emission rate with upper limit of 6 BEETS/block
+     - more require checks in general
+*/
+
 // Have fun reading it. Hopefully it's still bug-free
 contract BeethovenxMasterChef is Ownable {
     using SafeERC20 for IERC20;
