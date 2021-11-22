ff1563a1f5a336a11482d4acfddd98f2e4e6f90f
0xSkly <skly.beets@protonmail.com>
2021-11-22 17:07:43 +0100

    locking poc

--- contracts/governance/BeetsBar.sol
+++ contracts/governance/BeetsBar.sol
@@ -5,26 +5,61 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BeetsBar is ERC20("FreshBeets", "fBEETS") {
+contract BeetsBar is ERC20("FreshBeets", "fBEETS"), Ownable {
     using SafeERC20 for IERC20;
 
+    uint256 public constant LOCK_PENALTY_DENOMINATOR = 1000;
+
+    uint256 public lockPenalty;
     IERC20 public vestingToken;
+    uint256 public lockPeriod;
+    mapping(address => uint256) public userLockStartTs;
+    mapping(address => uint256) public userEligibleFreshBeets;
 
     event Enter(
         address indexed user,
         uint256 vestingInAmount,
-        uint256 mintedAmount
+        uint256 mintedAmount,
+        uint256 lockStartTs
     );
     event Leave(
         address indexed user,
         uint256 vestingOutAmount,
-        uint256 burnedAmount
+        uint256 burnedAmount,
+        uint256 lockStartTs
     );
     event ShareRevenue(uint256 amount);
+    event SetLockDuration(uint256 duration);
+    event SetLockPenalty(uint256 lockPenality);
 
-    constructor(IERC20 _vestingToken) {
+    constructor(
+        IERC20 _vestingToken,
+        uint256 _lockDuration,
+        uint256 _lockPenalty
+    ) {
+        require(
+            _lockPenalty <= (LOCK_PENALTY_DENOMINATOR / 2),
+            "Lock penalty can be at most 50% of the funds"
+        );
+
         vestingToken = _vestingToken;
+        lockPeriod = _lockDuration;
+        lockPenalty = _lockPenalty;
+    }
+
+    function setLockDuration(uint256 _lockDuration) external onlyOwner {
+        lockPeriod = _lockDuration;
+        emit SetLockDuration(_lockDuration);
+    }
+
+    function setLockPenalty(uint256 _lockPenalty) external onlyOwner {
+        require(
+            _lockPenalty <= (LOCK_PENALTY_DENOMINATOR / 2),
+            "Lock penalty can be at most 50% of the funds"
+        );
+        lockPenalty = _lockPenalty;
     }
 
     function enter(uint256 _amount) external {
@@ -48,8 +83,14 @@ contract BeetsBar is ERC20("FreshBeets", "fBEETS") {
 
                 mintAmount = shareOfFreshBeets;
             }
+
+            userEligibleFreshBeets[msg.sender] =
+                userEligibleFreshBeets[msg.sender] +
+                mintAmount;
+
             _mint(msg.sender, mintAmount);
-            emit Enter(msg.sender, _amount, mintAmount);
+            userLockStartTs[msg.sender] = block.timestamp;
+            emit Enter(msg.sender, _amount, mintAmount, block.timestamp);
         }
     }
 
@@ -59,12 +100,33 @@ contract BeetsBar is ERC20("FreshBeets", "fBEETS") {
                 address(this)
             );
             uint256 totalFreshBeets = totalSupply();
+
+            uint256 lockEndTs = userLockStartTs[msg.sender] + lockPeriod;
+
+            uint256 toWithdraw = _shareOfFreshBeets;
+            uint256 lockStart = userLockStartTs[msg.sender];
+            uint256 currentLockDuration = block.timestamp.sub(lockStart);
+
+            if (lockStart != 0 && currentLockDuration < lockPeriod) {
+                uint256 sharesBase = (numberOfShares *
+                    (LOCK_PENALTY_DENOMINATOR - lockPenalty)) /
+                    LOCK_PENALTY_DENOMINATOR;
+
+                toWithdraw =
+                    sharesBase +
+                    ((numberOfShares - sharesBase) *
+                        (currentLockDuration / lockPeriod));
+            }
+
             // Calculates the amount of vestingToken the fBeets are worth
-            uint256 amount = (_shareOfFreshBeets * totalVestedTokenSupply) /
+            uint256 amount = (toWithdraw * totalVestedTokenSupply) /
                 totalFreshBeets;
+
+            userEligibleFreshBeets[msg.sender] =
+                userEligibleFreshBeets[msg.sender] -
+                _shareOfFreshBeets;
             _burn(msg.sender, _shareOfFreshBeets);
             vestingToken.transfer(msg.sender, amount);
-
             emit Leave(msg.sender, amount, _shareOfFreshBeets);
         }
     }
@@ -72,5 +134,50 @@ contract BeetsBar is ERC20("FreshBeets", "fBEETS") {
     function shareRevenue(uint256 _amount) external {
         vestingToken.transferFrom(msg.sender, address(this), _amount);
         emit ShareRevenue(_amount);
+    }
+
+    function transfer(address _recipient, uint256 _amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        transferLockDuration(msg.sender, _recipient, _amount);
+        super.transfer(_recipient, _amount);
+        return true;
+    }
+
+    function transferFrom(
+        address _sender,
+        address _recipient,
+        uint256 _amount
+    ) public virtual override returns (bool) {
+        transferLockDuration(_sender, _recipient, _amount);
+        super.transferFrom(_sender, _recipient, _amount);
+        return true;
+    }
+
+    /**
+     * @dev Applies the senders lock duration to the recipient if its longer. Compares
+     *      balanceOf with amount transferred so it has to be called before the transfer
+     * @param _sender fBeets sender
+     * @param _recipient fBeets recipient
+     * @param _amount amount to transfer
+     *
+     */
+    function transferLockDuration(
+        address _sender,
+        address _recipient,
+        uint256 _amount
+    ) private {
+        uint256 senderLockDuration = userLockStartTs[_sender];
+        // if the lock duration of the sender is longer than recipient, we take sender lock duration
+        if (userLockStartTs[_recipient] < senderLockDuration) {
+            userLockStartTs[_recipient] = senderLockDuration;
+        }
+        // if sender has no more fBeets, reset lock start to 0
+        if (balanceOf(_sender) == _amount) {
+            userLockStartTs[_sender] = 0;
+        }
     }
 }
