42e89a6a91ace70eb53a993778031bd1dde9cb7a
0xSkly <skly.beets@protonmail.com>
2022-01-28 16:33:09 +0100

    Add master chef operator

---
+++ contracts/governance/MasterChefOperator.sol
@@ -0,0 +1,241 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "./Timelock.sol";
+import "../interfaces/IRewarder.sol";
+import "hardhat/console.sol";
+
+contract MasterChefOperator is AccessControl {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    bytes32 public constant STAGE_ROLE = keccak256("STAGING");
+    bytes32 public constant COMMIT_ROLE = keccak256("COMMITTING");
+
+    struct FarmModification {
+        uint256 pid;
+        uint256 allocationPoints;
+        IRewarder rewarder;
+        bool overwriteRewarder;
+    }
+
+    struct FarmAddition {
+        IERC20 lpToken;
+        uint256 allocationPoints;
+        IRewarder rewarder;
+    }
+
+    enum TransactionType {
+        QUEUE,
+        EXECUTE
+    }
+
+    // mapping eta => changes
+    mapping(uint256 => FarmAddition[]) public farmAdditions;
+    mapping(uint256 => FarmModification[]) public farmModifications;
+
+    // eta's which have already been used
+    mapping(uint256 => bool) public usedFarmChangeEtas;
+
+    EnumerableSet.UintSet private _farmModificationEtas;
+
+    Timelock public immutable timelock;
+    address public immutable masterChef;
+
+    constructor(
+        Timelock _timelock,
+        address _masterChef,
+        address admin,
+        address stagingAdmin
+    ) {
+        timelock = _timelock;
+        masterChef = _masterChef;
+        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+        _setupRole(COMMIT_ROLE, admin);
+        _setupRole(STAGE_ROLE, stagingAdmin);
+    }
+
+    function acceptTimelockAdmin() external onlyRole(COMMIT_ROLE) {
+        timelock.acceptAdmin();
+    }
+
+    function commitSetPendingTimelockAdmin(
+        address admin,
+        uint256 eta,
+        TransactionType txType
+    ) external onlyRole(COMMIT_ROLE) {
+        if (txType == TransactionType.QUEUE) {
+            timelock.queueTransaction(
+                address(timelock),
+                0,
+                "setPendingAdmin(address)",
+                abi.encode(admin),
+                eta
+            );
+        } else {
+            timelock.executeTransaction(
+                address(timelock),
+                0,
+                "setPendingAdmin(address)",
+                abi.encode(admin),
+                eta
+            );
+        }
+    }
+
+    function commitSetTreasuryAddress(
+        address treasury,
+        uint256 eta,
+        TransactionType txType
+    ) external onlyRole(COMMIT_ROLE) {
+        if (txType == TransactionType.QUEUE) {
+            timelock.queueTransaction(
+                masterChef,
+                0,
+                "treasury(address)",
+                abi.encode(treasury),
+                eta
+            );
+        } else {
+            timelock.executeTransaction(
+                masterChef,
+                0,
+                "treasury(address)",
+                abi.encode(treasury),
+                eta
+            );
+        }
+    }
+
+    function commitEmissionChange(
+        uint256 beetsPerBlock,
+        uint256 eta,
+        TransactionType txType
+    ) external onlyRole(COMMIT_ROLE) {
+        if (txType == TransactionType.QUEUE) {
+            timelock.queueTransaction(
+                masterChef,
+                0,
+                "updateEmissionRate(uint256)",
+                abi.encode(beetsPerBlock),
+                eta
+            );
+        } else {
+            timelock.executeTransaction(
+                masterChef,
+                0,
+                "updateEmissionRate(uint256)",
+                abi.encode(beetsPerBlock),
+                eta
+            );
+        }
+    }
+
+    function stageFarmAdditions(FarmAddition[] calldata farmsToAdd, uint256 eta)
+        external
+        updateEtas(eta)
+        onlyRole(STAGE_ROLE)
+    {
+        for (uint256 i = 0; i < farmsToAdd.length; i++) {
+            farmAdditions[eta].push(farmsToAdd[i]);
+        }
+    }
+
+    function stageFarmModifications(
+        FarmModification[] calldata farmsToEdit,
+        uint256 eta
+    ) external updateEtas(eta) onlyRole(STAGE_ROLE) {
+        for (uint256 i = 0; i < farmsToEdit.length; i++) {
+            farmModifications[eta].push(farmsToEdit[i]);
+        }
+    }
+
+    function commitFarmChanges(uint256 eta, TransactionType txType)
+        external
+        onlyRole(COMMIT_ROLE)
+    {
+        if (txType == TransactionType.QUEUE) {
+            queueFarmModifications(eta);
+        } else {
+            executeFarmModifications(eta);
+        }
+    }
+
+    function queueFarmModifications(uint256 eta) internal {
+        FarmModification[] storage farmEditTxs = farmModifications[eta];
+        for (uint256 i = 0; i < farmEditTxs.length; i++) {
+            timelock.queueTransaction(
+                masterChef,
+                0,
+                "set(uint256,uint256,address,bool)",
+                abi.encode(
+                    farmEditTxs[i].pid,
+                    farmEditTxs[i].allocationPoints,
+                    farmEditTxs[i].rewarder,
+                    farmEditTxs[i].overwriteRewarder
+                ),
+                eta
+            );
+        }
+        FarmAddition[] storage farmAddTxs = farmAdditions[eta];
+        for (uint256 i = 0; i < farmAddTxs.length; i++) {
+            timelock.queueTransaction(
+                masterChef,
+                0,
+                "add(uint256,address,address)",
+                abi.encode(
+                    farmAddTxs[i].allocationPoints,
+                    farmAddTxs[i].lpToken,
+                    farmAddTxs[i].rewarder
+                ),
+                eta
+            );
+        }
+
+        usedFarmChangeEtas[eta] = true;
+    }
+
+    function executeFarmModifications(uint256 eta) internal {
+        FarmModification[] storage farmEditTxs = farmModifications[eta];
+        for (uint256 i = 0; i < farmEditTxs.length; i++) {
+            timelock.executeTransaction(
+                masterChef,
+                0,
+                "set(uint256,uint256,address,bool)",
+                abi.encode(
+                    farmEditTxs[i].pid,
+                    farmEditTxs[i].allocationPoints,
+                    farmEditTxs[i].rewarder,
+                    farmEditTxs[i].overwriteRewarder
+                ),
+                eta
+            );
+        }
+
+        FarmAddition[] storage farmAddTxs = farmAdditions[eta];
+        for (uint256 i = 0; i < farmAddTxs.length; i++) {
+            timelock.executeTransaction(
+                masterChef,
+                0,
+                "add(uint256,address,address)",
+                abi.encode(
+                    farmAddTxs[i].allocationPoints,
+                    farmAddTxs[i].lpToken,
+                    farmAddTxs[i].rewarder
+                ),
+                eta
+            );
+        }
+    }
+
+    function farmModificationEtas() external view returns (uint256[] memory) {
+        return _farmModificationEtas.values();
+    }
+
+    modifier updateEtas(uint256 eta) {
+        require(!usedFarmChangeEtas[eta], "ETA already used, chose other eta");
+        _farmModificationEtas.add(eta);
+        _;
+    }
+}
---
+++ test/MasterchefOperator.test.ts
@@ -0,0 +1,276 @@
+import { advanceTime, bn, deployChef, deployContract, deployERC20Mock, duration, latest } from "./utilities"
+import { ethers } from "hardhat"
+import { BeethovenxMasterChef, BeethovenxToken, MasterChefOperator, TimeBasedMasterChefRewarder, Timelock } from "../types"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { expect } from "chai"
+import moment from "moment"
+
+describe("MasterChefOperator", function () {
+  let beets: BeethovenxToken
+  let chef: BeethovenxMasterChef
+  let timelock: Timelock
+  let operator: MasterChefOperator
+  let owner: SignerWithAddress
+  let alice: SignerWithAddress
+  let admin: SignerWithAddress
+  let stagingAdmin: SignerWithAddress
+  let rewarder: TimeBasedMasterChefRewarder
+  let treasury: SignerWithAddress
+
+  before(async function () {
+    const signers = await ethers.getSigners()
+    owner = signers[0]
+    admin = signers[5]
+    alice = signers[4]
+    treasury = signers[6]
+    stagingAdmin = signers[7]
+
+    beets = await deployContract("BeethovenxToken", [])
+  })
+
+  beforeEach(async function () {
+    timelock = await deployContract("Timelock", [admin.address, duration.hours("8")])
+    chef = await deployChef(beets.address, treasury.address, bn(6))
+    await chef.transferOwnership(timelock.address)
+    rewarder = await deployContract("TimeBasedMasterChefRewarder", [beets.address, bn(1), chef.address])
+
+    operator = await deployContract("MasterChefOperator", [timelock.address, chef.address, admin.address, stagingAdmin.address])
+    await timelock.connect(admin).setPendingAdmin(operator.address)
+    await operator.connect(admin).acceptTimelockAdmin()
+  })
+
+  it("batches farm modifications", async () => {
+    const lpToken = await deployERC20Mock("some token", "st", bn(1000))
+    const anotherLpToken = await deployERC20Mock("another token", "at", bn(1000))
+    const yetAnotherLpToken = await deployERC20Mock("yet another token", "at", bn(1000))
+
+    const etaFarmAdd = await createEta()
+    await operator.connect(stagingAdmin).stageFarmAdditions(
+      [
+        { lpToken: beets.address, allocationPoints: 10, rewarder: ethers.constants.AddressZero },
+        { lpToken: lpToken.address, allocationPoints: 20, rewarder: rewarder.address },
+        { lpToken: anotherLpToken.address, allocationPoints: 30, rewarder: ethers.constants.AddressZero },
+      ],
+      etaFarmAdd
+    )
+
+    await operator
+      .connect(stagingAdmin)
+      .stageFarmAdditions([{ lpToken: yetAnotherLpToken.address, allocationPoints: 40, rewarder: ethers.constants.AddressZero }], etaFarmAdd)
+
+    const farmAddTx0 = await operator.farmAdditions(etaFarmAdd, 0)
+    const farmAddTx1 = await operator.farmAdditions(etaFarmAdd, 1)
+    const farmAddTx2 = await operator.farmAdditions(etaFarmAdd, 2)
+    const farmAddTx3 = await operator.farmAdditions(etaFarmAdd, 3)
+
+    expect(farmAddTx0.lpToken).to.equal(beets.address)
+    expect(farmAddTx0.allocationPoints).to.equal(10)
+    expect(farmAddTx0.rewarder).to.equal(ethers.constants.AddressZero)
+
+    expect(farmAddTx1.lpToken).to.equal(lpToken.address)
+    expect(farmAddTx1.allocationPoints).to.equal(20)
+    expect(farmAddTx1.rewarder).to.equal(rewarder.address)
+
+    expect(farmAddTx2.lpToken).to.equal(anotherLpToken.address)
+    expect(farmAddTx2.allocationPoints).to.equal(30)
+    expect(farmAddTx2.rewarder).to.equal(ethers.constants.AddressZero)
+
+    expect(farmAddTx3.lpToken).to.equal(yetAnotherLpToken.address)
+    expect(farmAddTx3.allocationPoints).to.equal(40)
+    expect(farmAddTx3.rewarder).to.equal(ethers.constants.AddressZero)
+
+    // lets queue it up
+    await operator.connect(admin).commitFarmChanges(etaFarmAdd, 0)
+
+    await advanceTime(duration.hours("10").toNumber())
+
+    // and execute it
+    await operator.connect(admin).commitFarmChanges(etaFarmAdd, 1)
+
+    expect(await chef.poolLength()).to.equal(4)
+
+    expect(await chef.lpTokens(0)).to.equal(beets.address)
+    const poolInfo0 = await chef.poolInfo(0)
+    expect(poolInfo0.allocPoint).to.equal(10)
+    expect(await chef.rewarder(0)).to.equal(ethers.constants.AddressZero)
+
+    expect(await chef.lpTokens(1)).to.equal(lpToken.address)
+    const poolInfo1 = await chef.poolInfo(1)
+    expect(poolInfo1.allocPoint).to.equal(20)
+    expect(await chef.rewarder(1)).to.equal(rewarder.address)
+
+    expect(await chef.lpTokens(2)).to.equal(anotherLpToken.address)
+    const poolInfo2 = await chef.poolInfo(2)
+    expect(poolInfo2.allocPoint).to.equal(30)
+    expect(await chef.rewarder(2)).to.equal(ethers.constants.AddressZero)
+
+    expect(await chef.lpTokens(3)).to.equal(yetAnotherLpToken.address)
+    const poolInfo3 = await chef.poolInfo(3)
+    expect(poolInfo3.allocPoint).to.equal(40)
+    expect(await chef.rewarder(3)).to.equal(ethers.constants.AddressZero)
+
+    // now lets edit some farms
+
+    const etaFarmEdit = await createEta()
+    await operator.connect(stagingAdmin).stageFarmModifications(
+      [
+        {
+          allocationPoints: 5,
+          pid: 0,
+          rewarder: ethers.constants.AddressZero,
+          overwriteRewarder: false,
+        },
+      ],
+      etaFarmEdit
+    )
+
+    await operator.connect(stagingAdmin).stageFarmModifications(
+      [
+        {
+          allocationPoints: 2,
+          pid: 1,
+          rewarder: ethers.constants.AddressZero,
+          overwriteRewarder: true,
+        },
+      ],
+      etaFarmEdit
+    )
+
+    const farmEditTx0 = await operator.farmModifications(etaFarmEdit, 0)
+    const farmEditTx1 = await operator.farmModifications(etaFarmEdit, 1)
+
+    expect(farmEditTx0.pid).to.equal(0)
+    expect(farmEditTx0.allocationPoints).to.equal(5)
+    expect(farmEditTx0.rewarder).to.equal(ethers.constants.AddressZero)
+    expect(farmEditTx0.overwriteRewarder).to.equal(false)
+
+    expect(farmEditTx1.pid).to.equal(1)
+    expect(farmEditTx1.allocationPoints).to.equal(2)
+    expect(farmEditTx0.rewarder).to.equal(ethers.constants.AddressZero)
+    expect(farmEditTx1.overwriteRewarder).to.equal(true)
+
+    // lets queue it up
+    await operator.connect(admin).commitFarmChanges(etaFarmEdit, 0)
+
+    await advanceTime(duration.hours("10").toNumber())
+
+    // and execute it
+    await operator.connect(admin).commitFarmChanges(etaFarmEdit, 1)
+
+    const poolInfo0AfterEdit = await chef.poolInfo(0)
+    expect(poolInfo0AfterEdit.allocPoint).to.equal(5)
+    expect(await chef.rewarder(0)).to.equal(ethers.constants.AddressZero)
+
+    const poolInfo1AfterEdit = await chef.poolInfo(1)
+    expect(poolInfo1AfterEdit.allocPoint).to.equal(2)
+    expect(await chef.rewarder(1)).to.equal(ethers.constants.AddressZero)
+  })
+
+  it("only allows users with role STAGING to stage farm changes", async () => {
+    await expect(
+      operator.connect(alice).stageFarmModifications(
+        [
+          {
+            allocationPoints: 2,
+            pid: 1,
+            rewarder: ethers.constants.AddressZero,
+            overwriteRewarder: true,
+          },
+        ],
+        await createEta()
+      )
+    ).to.be.reverted
+  })
+
+  it("only allows user with COMMITTER role to commit farm changes", async () => {
+    const etaFarmEdit = await createEta()
+    await operator.connect(stagingAdmin).stageFarmModifications(
+      [
+        {
+          allocationPoints: 5,
+          pid: 0,
+          rewarder: ethers.constants.AddressZero,
+          overwriteRewarder: false,
+        },
+      ],
+      etaFarmEdit
+    )
+
+    await expect(operator.connect(stagingAdmin).commitFarmChanges(etaFarmEdit, 0)).to.be.reverted
+    await expect(operator.connect(alice).commitFarmChanges(etaFarmEdit, 0)).to.be.reverted
+  })
+
+  it("reverts when staging with an already used eta", async () => {
+    const lpToken = await deployERC20Mock("some token", "st", bn(1000))
+    const yetAnotherLpToken = await deployERC20Mock("yet another token", "at", bn(1000))
+    const eta = await createEta()
+    await operator
+      .connect(stagingAdmin)
+      .stageFarmAdditions([{ lpToken: lpToken.address, allocationPoints: 20, rewarder: rewarder.address }], eta)
+
+    await operator.connect(admin).commitFarmChanges(eta, 0)
+
+    await expect(
+      operator
+        .connect(stagingAdmin)
+        .stageFarmAdditions([{ lpToken: yetAnotherLpToken.address, allocationPoints: 40, rewarder: ethers.constants.AddressZero }], eta)
+    ).to.be.revertedWith("ETA already used, chose other eta")
+  })
+
+  it("sets peding timelock admin", async () => {
+    const eta = await createEta()
+
+    await operator.connect(admin).commitSetPendingTimelockAdmin(alice.address, eta, 0)
+    await advanceTime(duration.hours("10").toNumber())
+    await operator.connect(admin).commitSetPendingTimelockAdmin(alice.address, eta, 1)
+
+    await timelock.connect(alice).acceptAdmin()
+    expect(await timelock.admin()).to.equal(alice.address)
+  })
+
+  it("only allows user with COMMITTER role to set pending timelock admin", async () => {
+    const eta = await createEta()
+    await expect(operator.connect(stagingAdmin).commitSetPendingTimelockAdmin(alice.address, eta, 0)).to.be.reverted
+    await expect(operator.connect(alice).commitSetPendingTimelockAdmin(alice.address, eta, 0)).to.be.reverted
+  })
+
+  it("changes token emissions", async () => {
+    const eta = await createEta()
+
+    await operator.connect(admin).commitEmissionChange(bn(2), eta, 0)
+    await advanceTime(duration.hours("10").toNumber())
+    await operator.connect(admin).commitEmissionChange(bn(2), eta, 1)
+
+    expect(await chef.beetsPerBlock()).to.equal(bn(2))
+  })
+
+  it("only allows user with COMMITTER role to change emissions", async () => {
+    const eta = await createEta()
+
+    await expect(operator.connect(stagingAdmin).commitEmissionChange(bn(2), eta, 0)).to.be.reverted
+    await expect(operator.connect(alice).commitEmissionChange(bn(2), eta, 0)).to.be.reverted
+  })
+
+  it("changes treasury address", async () => {
+    const eta = await createEta()
+
+    await operator.connect(admin).commitSetTreasuryAddress(alice.address, eta, 0)
+    await advanceTime(duration.hours("10").toNumber())
+    await operator.connect(admin).commitSetTreasuryAddress(alice.address, eta, 1)
+
+    expect(await chef.treasuryAddress()).to.equal(alice.address)
+  })
+
+  it("only allows user with COMMITTER role to change treasury address", async () => {
+    const eta = await createEta()
+    await expect(operator.connect(stagingAdmin).commitSetTreasuryAddress(alice.address, eta, 0)).to.be.reverted
+    await expect(operator.connect(alice).commitSetTreasuryAddress(alice.address, eta, 0)).to.be.reverted
+  })
+
+  async function createEta(hours: number = 10) {
+    return moment
+      .unix((await latest()).toNumber())
+      .add(hours, "hours")
+      .unix()
+  }
+})
