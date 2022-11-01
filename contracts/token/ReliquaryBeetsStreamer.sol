866be05cd3aeedbbb034325c9154fc0171f07bec
franz <franzns11@gmail.com>
2022-11-01 15:11:13 +0100

    add reliquary streamer

---
+++ contracts/token/ReliquaryBeetsStreamer.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./BeethovenxMasterChef.sol";
+import "hardhat/console.sol";
+
+contract ReliquaryBeetsStreamer is
+    ERC20("ReliquaryStreamerBPT", "rqBPT"),
+    Ownable
+{
+    using SafeERC20 for IERC20;
+
+    uint256 public constant MAX_SUPPLY = 10; // 10 tokens only
+
+    BeethovenxMasterChef public masterchef;
+    address public reliquaryAddress;
+    uint256 public masterchefPoolId;
+
+    constructor(
+        BeethovenxMasterChef _masterchef,
+        uint256 _masterchefPoolId,
+        address _reliquary
+    ) {
+        masterchef = _masterchef;
+        masterchefPoolId = _masterchefPoolId;
+        reliquaryAddress = _reliquary;
+    }
+
+    // mints one BPT and deposits it into the masterchef
+    function deposit() external onlyOwner {
+        require(
+            totalSupply() + 1 <= MAX_SUPPLY,
+            "rqBPT::mint: cannot exceed max supply"
+        );
+        _mint(address(this), 1);
+        _approve(address(this), address(masterchef), 1);
+        masterchef.deposit(masterchefPoolId, 1, address(this));
+    }
+
+    function harvestToReliquary() external onlyOwner {
+        masterchef.harvest(masterchefPoolId, reliquaryAddress);
+    }
+}
