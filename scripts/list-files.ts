4b5ca9728c55a2d414b2b858b8150e9706261fcb
franz <franzns11@gmail.com>
2024-12-03 16:45:06 +0100

    init

--- scripts/list-files.ts
+++ scripts/list-files.ts
@@ -1,18 +1,21 @@
-import fs from "fs"
-import path from "path"
+import fs from 'fs'
+import path from 'path'
 
 export const getSolidityFileNames = (directory: string): string[] => {
-  const filesInDirectory = fs.readdirSync(directory)
-  const files = []
-  for (const file of filesInDirectory) {
-    const absolute = path.join(directory, file)
-    if (fs.statSync(absolute).isDirectory()) {
-      files.push(...getSolidityFileNames(absolute))
-    } else {
-      if (file.endsWith(".sol")) {
-        files.push(file.replace(".sol", ""))
-      }
+    const filesInDirectory = fs.readdirSync(directory)
+    const files = []
+    for (const file of filesInDirectory) {
+        const absolute = path.join(directory, file)
+        if (fs.statSync(absolute).isDirectory()) {
+            files.push(...getSolidityFileNames(absolute))
+        } else {
+            if (file.endsWith('.sol')) {
+                files.push(file.replace('.sol', ''))
+            }
+            if (file.endsWith('.vy')) {
+                files.push(file.replace('.vy', ''))
+            }
+        }
     }
-  }
-  return files
+    return files
 }
